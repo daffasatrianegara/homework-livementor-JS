3f6fe5a57ae6d1a3ab98f0453bd932bad2c3f056
Wisnu Andika <124800648+pendragonnn@users.noreply.github.com>
2023-09-17 14:10:39 +0700

    <wisnu> add persegi panjang class

--- Trapesium.js
+++ Trapesium.js
@@ -16,6 +16,28 @@ class Trapesium {
     }
 }
 
+class PersegiPanjang {
+  constructor(panjang, lebar) {
+    this.panjang = panjang;
+    this.lebar = lebar;
+  }
+
+  // Menghitung luas
+  hitungLuas() {
+    return this.panjang * this.lebar;
+  }
+  
+  // Menghitung keliling
+  hitungKeliling() {
+    return 2 * (this.panjang + this.lebar)
+  }
+}
+
 const hitungTrapesium = new Trapesium(5, 7, 8, 6, 4);
+const persegiPanjang = new PersegiPanjang(2,3)
+
+
 console.log("Luas trapesium: " + hitungTrapesium.hitungLuas());
 console.log("Keliling trapesium: " + hitungTrapesium.hitungKeliling());
+console.log(`Keliling persegi panjang adalah: ${ persegiPanjang.hitungKeliling()}`)
+console.log(`Luas persegi panjang adalah: ${ persegiPanjang.hitungLuas()}`)
